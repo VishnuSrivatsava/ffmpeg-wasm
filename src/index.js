8c61df5201a6f860b2b6bf90aa9bb9f52ddcd66e
Vishnu Srivatsava <49309611+VishnuSrivatsava@users.noreply.github.com>
2023-03-18 23:36:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css"
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
 import "antd/dist/antd.css"
-
+if (!crossOriginIsolated) SharedArrayBuffer = ArrayBuffer;
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
 
